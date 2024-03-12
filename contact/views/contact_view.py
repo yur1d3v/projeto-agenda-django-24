da137e4ae080e15d5fb7e479e4d9a39581557ac6
Yuri Costa <yuricosta2319@gmail.com>
2024-03-12 19:29:40 -0300

    Usando a request no template para pegar o valor de GET

--- contact/views/contact_view.py
+++ contact/views/contact_view.py
@@ -1,4 +1,5 @@
-from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
 from contact.models import Contact
 
 
@@ -6,10 +7,35 @@ def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
         .order_by('-id')[10:20]
-
     context = {
         'contacts': contacts,
         'site_title': 'Contatos - '
+    }
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    if search_value == '':
+        return redirect('contact:index')
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
@@ -25,12 +51,10 @@ def contact(request, contact_id):
         Contact, pk=contact_id, show=True
     )
     site_title = f'{single_contact.first_name} {single_contact.last_name} - '
-
     context = {
         'contact': single_contact,
         'site_title': site_title
     }
-
     return render(
         request,
         'contact/contact.html',
