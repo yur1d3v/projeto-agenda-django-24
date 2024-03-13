0aa94bcd75b4c15ccfa6521fffac8d8ef0627480
Yuri Costa <yuricosta2319@gmail.com>
2024-03-13 20:19:48 -0300

    csrf_token

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,15 +1,15 @@
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
-
 from contact.models import Contact
 
 
 def create(request):
+    post = request.POST
+
     context = {
 
     }
-
     return render(
         request,
         'contact/create.html',
