0dac4c534c801f9ad7c2b5b6cf023bde66ed1423
Yuri Costa <yuricosta2319@gmail.com>
2024-03-18 19:54:59 -0300

    Trabalhando com ImageField no template e nos forms

--- contact/forms.py
+++ contact/forms.py
@@ -1,46 +1,31 @@
 from django import forms
 from django.core.exceptions import ValidationError
+
 from . import models
 
 
 class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput(
+    picture = forms.ImageField(
+        widget=forms.FileInput(
             attrs={
-                'class': 'classe-a classe-b',
-                'placeholder': 'Aqui veio do init',
+                'accept': 'image/*',
             }
-        ),
-        label='Primeiro Nome',
-        help_text='Texto de ajuda para seu usuário',
+        )
     )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # self.fields['first_name'].widget.attrs.update({
-        #     'class': 'classe-a classe-b',
-        #     'placeholder': 'Aqui veio do init',
-        # })
 
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
             'email', 'description', 'category',
+            'picture',
         )
-        # widgets = {
-        #     'first_name': forms.TextInput(
-        #         attrs={
-        #             'class': 'classe-a classe-b',
-        #             'placeholder': 'Escreva aqui',
-        #         }
-        #     )
-        # }
 
     def clean(self):
         cleaned_data = self.cleaned_data
         first_name = cleaned_data.get('first_name')
         last_name = cleaned_data.get('last_name')
+
         if first_name == last_name:
             msg = ValidationError(
                 'Primeiro nome não pode ser igual ao segundo',
@@ -48,10 +33,12 @@ class ContactForm(forms.ModelForm):
             )
             self.add_error('first_name', msg)
             self.add_error('last_name', msg)
+
         return super().clean()
 
     def clean_first_name(self):
         first_name = self.cleaned_data.get('first_name')
+
         if first_name == 'ABC':
             self.add_error(
                 'first_name',
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-
 from contact.forms import ContactForm
 from contact.models import Contact
 
@@ -9,28 +8,24 @@ def create(request):
     form_action = reverse('contact:create')
 
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, request.FILES)
 
         context = {
             'form': form,
             'form_action': form_action,
         }
-
         if form.is_valid():
             contact = form.save()
             return redirect('contact:update', contact_id=contact.pk)
-
         return render(
             request,
             'contact/create.html',
             context
         )
-
     context = {
         'form': ContactForm(),
         'form_action': form_action,
     }
-
     return render(
         request,
         'contact/create.html',
@@ -45,28 +40,24 @@ def update(request, contact_id):
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
-        form = ContactForm(request.POST, instance=contact)
+        form = ContactForm(request.POST, request.FILES, instance=contact)
 
         context = {
             'form': form,
             'form_action': form_action,
         }
-
         if form.is_valid():
             contact = form.save()
             return redirect('contact:update', contact_id=contact.pk)
-
         return render(
             request,
             'contact/create.html',
             context
         )
-
     context = {
         'form': ContactForm(instance=contact),
         'form_action': form_action,
     }
-
     return render(
         request,
         'contact/create.html',
@@ -79,11 +70,9 @@ def delete(request, contact_id):
         Contact, pk=contact_id, show=True
     )
     confirmation = request.POST.get('confirmation', 'no')
-
     if confirmation == 'yes':
         contact.delete()
         return redirect('contact:index')
-
     return render(
         request,
         'contact/contact.html',
