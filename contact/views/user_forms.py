eb0bc41ce8b06826b23c224ab81786451f6b1790
Yuri Costa <yuricosta2319@gmail.com>
2024-03-20 20:22:20 -0300

    Criando o sistema de login e logout + AuthenticationForm

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,4 +1,5 @@
-from django.contrib import messages
+from django.contrib import auth, messages
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render
 
 from contact.forms import RegisterForm
@@ -8,11 +9,10 @@ def register(request):
     form = RegisterForm()
     if request.method == 'POST':
         form = RegisterForm(request.POST)
-
         if form.is_valid():
             form.save()
             messages.success(request, 'Usuário registrado')
-            return redirect('contact:index')
+            return redirect('contact:login')
 
     return render(
         request,
@@ -21,3 +21,30 @@ def register(request):
             'form': form
         }
     )
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'Logado com sucesso!')
+            return redirect('contact:index')
+        messages.error(request, 'Login inválido')
+
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form
+        }
+    )
+
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
