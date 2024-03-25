51e9b49fbda8900e00a69aa61bff19a2527b3069
Yuri Costa <yuricosta2319@gmail.com>
2024-03-25 19:57:25 -0300

    Ocultando links de update e delete de não owner dos contatos

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth import password_validation
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+
 from . import models
 
 
@@ -12,7 +13,8 @@ class ContactForm(forms.ModelForm):
             attrs={
                 'accept': 'image/*',
             }
-        )
+        ),
+        required=False
     )
 
     class Meta:
@@ -27,6 +29,7 @@ class ContactForm(forms.ModelForm):
         cleaned_data = self.cleaned_data
         first_name = cleaned_data.get('first_name')
         last_name = cleaned_data.get('last_name')
+
         if first_name == last_name:
             msg = ValidationError(
                 'Primeiro nome não pode ser igual ao segundo',
@@ -34,10 +37,12 @@ class ContactForm(forms.ModelForm):
             )
             self.add_error('first_name', msg)
             self.add_error('last_name', msg)
+
         return super().clean()
 
     def clean_first_name(self):
         first_name = self.cleaned_data.get('first_name')
+
         if first_name == 'ABC':
             self.add_error(
                 'first_name',
@@ -46,6 +51,7 @@ class ContactForm(forms.ModelForm):
                     code='invalid'
                 )
             )
+
         return first_name
 
 
@@ -69,6 +75,7 @@ class RegisterForm(UserCreationForm):
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
+
         if User.objects.filter(email=email).exists():
             self.add_error(
                 'email',
